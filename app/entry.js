508d54b2e5e4e9e2b59ffb8c2c391cb734b2dd24
Soichiro-Yoshimura <yoshimura@soichiro>
2016-02-22 18:34:59 +0900

    first commit

---
+++ app/entry.js
@@ -0,0 +1,7 @@
+'use strict';
+let dc = require('damage-calc');
+document.writeln(
+  '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
+  dc.effectiveDamage(100, 50, 30) + '</p>');
+document.writeln(
+  '<p>' + require('crypto').randomBytes(8).toString('hex') + '</p>');
