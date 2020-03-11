fb4cdc82112c0fe7b83ce566fbf086d2299a18cc
yamacho <yamacho@users.noreply.github.com>
2020-03-11 16:49:29 +0900

    ブロック要素をアニメーションする処理を追加

--- app/entry.js
+++ app/entry.js
@@ -1,7 +1,9 @@
 'use strict';
-import dc from 'damage-calc';
-import crypto from 'crypto';
-const root = document.getElementById('root');
-root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
-  + dc.effectiveDamage(100, 50, 30) + '</p><p>'
-  + crypto.randomBytes(8).toString('hex') + '</p>';
+import $ from 'jquery';
+const block = $('#block');
+const scalingButton = $('#scaling-button');
+
+scalingButton.click(() => {
+  block.animate({ width: '200pt', height: '"200pt' }, 2000);
+  block.animate({ width: '100pt', height: '"100pt' }, 2000);
+});
